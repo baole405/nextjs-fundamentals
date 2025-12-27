595d7dba519ac71c3e5ed97835b9a604a84a2a12
baole405 <baolttse184569@fpt.edu.vn>
2025-12-27 10:09:47 +0700

    build: ignore types

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,11 @@ const nextConfig: NextConfig = {
   experimental: {
     dynamicIO: true,
   },
+  // ignore ts errors
   typescript: {
     ignoreBuildErrors: true,
   },
+  // ignore eslint errors
   eslint: {
     ignoreDuringBuilds: true,
   },
